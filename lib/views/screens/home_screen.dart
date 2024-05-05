9ac9388b101f3110c40c8e7c52aa0bffa03e77fc
kyaw min khant <56061215+MgKyawLay@users.noreply.github.com>
2024-05-05 12:19:53 +0630

    bottom navigation

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,12 +1,45 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:swifty_text_flutter/views/screens/chat_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  void _handleOnTap(int index) async {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  final List<Widget> _screens = <Widget>[
+    ChatScreen(),
+    Icon(EvaIcons.people),
+    Icon(EvaIcons.settings2),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Home"),
+    return Scaffold(
+      body: _screens.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _handleOnTap,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(EvaIcons.messageCircleOutline), label: "Chat"),
+          BottomNavigationBarItem(
+              icon: Icon(EvaIcons.peopleOutline), label: "People"),
+          BottomNavigationBarItem(
+              icon: Icon(EvaIcons.settings2Outline), label: "Setting"),
+        ],
+      ),
     );
   }
 }
